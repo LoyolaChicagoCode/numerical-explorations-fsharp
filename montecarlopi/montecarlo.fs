6e67864f47b691c46b7c71b7a15a04745e1e53d6
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-02-17 09:57:46 -0600

    a bit of cleanup to make this more like the Haskell version

--- montecarlopi/montecarlo.fs
+++ montecarlopi/montecarlo.fs
@@ -1,6 +1,5 @@
 open System
 open System.Diagnostics
-
 let MonteCarloPi(n : int64) : double =
     let generator = System.Random()
 
@@ -9,20 +8,27 @@ let MonteCarloPi(n : int64) : double =
     let incircle (x, y) =
         square(x) + square(y) <= 1.0
 
-    let darts = seq { for i in 1L .. n -> (generator.NextDouble(), generator.NextDouble()) }
+    let darts = seq { for i in 1L .. n -> (generator.NextDouble(), generator.NextDouble()) } 
 
-    let dartsInCircle = Seq.filter incircle darts
-    4.0 * double(Seq.length dartsInCircle) / double(Seq.length darts)
+    let dartsInCircle = darts |> Seq.filter incircle |> Seq.length
+    4.0 * double(dartsInCircle) / double(n)
+
+let SeqMax(n : int64) : int64 =
+    let darts = seq { for i in 1L .. n -> i }
+    darts |> Seq.max
 
 [<EntryPoint>]
 let main argv = 
     let n = int64(argv.[0])
-    Console.WriteLine("{0} Darts", n)
+    System.Console.WriteLine("{0} Darts", n)
     let sqr x : double = x * x
     let timer = new System.Diagnostics.Stopwatch()
     timer.Start()
     printfn "Monte Carlo %A" (MonteCarloPi n)
+    timer.Stop()
+    printfn "Elapsed Time: %i" timer.ElapsedMilliseconds
+    timer.Reset();
+    timer.Start();
+    printfn "SeqMax (to determine overheads of random generation) %A" (SeqMax n)
     printfn "Elapsed Time: %i" timer.ElapsedMilliseconds
     0
-
-
